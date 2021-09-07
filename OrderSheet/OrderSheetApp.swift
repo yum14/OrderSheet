db69ce686ac8c18f4a7491a336b23260ffb92023
yum14 <yumurata1214@gmail.com>
2021-09-07 22:07:22 +0900

    :tada: #1 add project.

---
+++ OrderSheet/OrderSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderSheetApp.swift
+//  OrderSheet
+//
+//  Created by yum on 2021/09/07.
+//
+
+import SwiftUI
+
+@main
+struct OrderSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
