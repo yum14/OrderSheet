072a8b7822f72ef3888f415c714a793fd609ac48
yum14 <yumurata1214@gmail.com>
2021-09-08 23:41:31 +0900

    :bug: #7 プレビューのDate変換を修正

--- OrderSheet/ViewComponent/OrderList.swift
+++ OrderSheet/ViewComponent/OrderList.swift
@@ -31,10 +31,11 @@ struct OrderList: View {
 
 struct OrderList_Previews: PreviewProvider {
     static var previews: some View {
-        let template = "yyyy/MM/dd HH:mm:ss Z"
-        let orders = [Order(name: "オーダー1", createdAt: DateUtility.toDate(dateString: "2021/01/01 01:00:00 +09:00", template: template)),
-                      Order(name: "オーダー2", createdAt: DateUtility.toDate(dateString: "2021/01/01 12:00:00 +09:00", template: template)),
-                      Order(name: "オーダー3", createdAt: DateUtility.toDate(dateString: "2021/01/02 01:00:00 +09:00", template: template))]
+        let template = "yyyy/MM/dd HH:mm:ss"
+        let orders = [Order(name: "オーダー1", createdAt: DateUtility.toDate(dateString: "2021/01/01 01:00:00", template: template)),
+                      Order(name: "オーダー2", createdAt: DateUtility.toDate(dateString: "2021/01/01 12:00:00", template: template)),
+                      Order(name: "オーダー3", createdAt: DateUtility.toDate(dateString: "2021/01/02 01:00:00", template: template))
+        ]
         
         OrderList(orders: orders)
     }
