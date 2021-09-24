8c613d036caaacb62e657d1dfa61e6a3ff6131f2
yum14 <yumurata1214@gmail.com>
2021-09-24 21:44:28 +0900

    :recycle: podからswift packageに移行

---
+++ OrderSheetUITests/OrderSheetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OrderSheetUITestsLaunchTests.swift
+//  OrderSheetUITests
+//
+//  Created by yum on 2021/09/24.
+//
+
+import XCTest
+
+class OrderSheetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
